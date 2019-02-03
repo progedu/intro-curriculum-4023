2ef665bb2ad563beefe1f817c98d292c320e7c6c
albertGH1996 <windowsrxp@gmail.com>
2019-02-03 22:50:40 +0900

    リダイレクトURLを修正

--- routes/auth0-login.js
+++ routes/auth0-login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 var AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID || 'eKWbdnubCoWrjqq8KMuMX6fwmu4eoNG0';
-var AUTH0_CALLBACK_URL = process.env.HEROKU_URL || 'http://localhost:8000/auth/auth0';
+var AUTH0_CALLBACK_URL = process.env.HEROKU_URL + 'auth/auth0' || 'http://localhost:8000/auth/auth0';
 router.get('/', (req, res, next) => {
   res.redirect('https://albertgh1996.auth0.com/v2/logout?client_id=' + AUTH0_CLIENT_ID + '&returnTo=' + AUTH0_CALLBACK_URL);
 });
