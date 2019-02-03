70a36adbeb3ac719c93f4e7534fd76275e00fed7
albertGH1996 <windowsrxp@gmail.com>
2019-02-03 22:39:19 +0900

    /routes/auth0-login.jsを追加

---
+++ routes/auth0-login.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+var AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID || 'eKWbdnubCoWrjqq8KMuMX6fwmu4eoNG0';
+var AUTH0_CALLBACK_URL = process.env.HEROKU_URL || 'http://localhost:8000/auth/auth0';
+router.get('/', (req, res, next) => {
+  res.redirect('https://albertgh1996.auth0.com/v2/logout?client_id=' + AUTH0_CLIENT_ID + '&returnTo=' + AUTH0_CALLBACK_URL);
+});
+
+module.exports = router;
