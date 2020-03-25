a49fd42e140f6c23d2c1bac3e2934bd7c5b03479
yos10 <yos.ohtake+git@gmail.com>
2020-03-25 14:15:20 +0900

    ファビコン追加

---
+++ routes/favicon.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+router.get('/', (req, res, next) => {
+  res.writeHead(200, {
+    'Content-Type': 'image/vnd.microsoft.icon'
+  });
+  const favicon = fs.readFileSync(__dirname + '/public/favicon.ico');
+  res.end(favicon);
+});
+
+module.exports = router;
