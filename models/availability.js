73fc445344fa7a014b3473526a1d4307659a3dfd
hakataminamiWS <hakataminami.web.service@gmail.com>
2020-07-30 16:37:10 +0900

    google oauth 認証テスト

--- models/availability.js
+++ models/availability.js
@@ -9,7 +9,7 @@ const Availability = loader.database.define('availabilities', {
     allowNull: false
   },
   userId: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.STRING,
     primaryKey: true,
     allowNull: false
   },
