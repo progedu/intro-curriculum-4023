d088889a880ef8c92f986ffc5cdfe69cbb69882a
ck-n2020 <ckckck_009@yahoo.co.jp>
2021-03-11 23:41:07 +0900

    DBへのSSL接続設定

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,7 +3,11 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
   {
-    operatorsAliases: false
+    operatorsAliases: false,
+    // DBにSSL接続する
+    dialectOptions: {
+      ssl: true
+    }
   });
 
 module.exports = {
